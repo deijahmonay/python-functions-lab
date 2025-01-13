eb9da2b353eefff7dad63c36c00549dae0b4ab12
deijahmonay <deijah@shack15.com>
2025-01-13 15:02:49 -0800

    pass exercise 9

--- exercises.py
+++ exercises.py
@@ -153,7 +153,19 @@ print('Exercise 8:', product(2, 4, 3))
 # two numbers and a string representing an operation ('add', 'subtract', 'multiply', 'divide'). 
 # Perform the provided operation on the two numbers. In operations where the order of numbers is important, 
 # treat the first parameter as the first operand and the second parameter as the second operand.
-#
+def basic_calculator(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."  
+        return num1 / num2
+    else:
+        return "Error: Invalid operation." 
 # Examples:
 # basic_calculator(10, 5, 'subtract') should return 5.
 # basic_calculator(10, 5, 'add') should return 15.
@@ -165,5 +177,8 @@ print('Exercise 8:', product(2, 4, 3))
 
 
 print('Exercise 9 Result:', basic_calculator(10, 5, "subtract"))
+print('Exercise 9 Result:', basic_calculator(10, 5, "add"))
+print('Exercise 9 Result:', basic_calculator(10, 5, "multiply"))
+print('Exercise 9 Result:', basic_calculator(10, 5, "divide"))
 
 
